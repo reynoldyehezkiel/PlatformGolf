3bfaa7ca72f7fa2e791916ee0dc8172c3b79fa2f
reynoldyehezkiel <73789263+reynoldyehezkiel@users.noreply.github.com>
2021-11-17 14:23:01 +0700

    Update PlayerManager.cs

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -14,13 +14,7 @@ public class PlayerManager : MonoBehaviour
         isGameOver = false;
         isGameFinish = false;
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         if(isGameOver)
